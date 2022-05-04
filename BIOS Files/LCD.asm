7728d4a3eace80cc91b482d846e776001ea531dc
Gabriel Womelsdorf <gabriel.womelsdorf.work@gmail.com>
2022-05-04 18:58:40 -0400

    New LCD

---
+++ BIOS Files/LCD.asm
@@ -0,0 +1,41 @@
+org $00
+
+nop
+nop
+nop
+;Add sufficient delay
+
+out (0),%00000011
+out (0),%00100011
+;Writes $02
+
+nop
+nop
+
+out (0),%10000011
+out (0),%00100011
+
+nop
+nop
+
+out (0),%00000011
+out (0),%11100011
+
+nop
+nop
+
+out (0),%00000011
+out (0),%01100011
+
+nop
+nop
+
+out (0),%00000011
+out (0),%00010011
+
+nop
+nop
+
+out (0),%10000011
+out (0),%00000011
+
